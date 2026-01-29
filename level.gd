5e9ecd314213348029f1d1974ec16c22b2e04418
Fuzzlewuzzlekins <47910839+Fuzzlewuzzlekins@users.noreply.github.com>
2026-01-29 13:47:57 +0100

    Add pause menu w/ reset and quit, update tile set

--- level.gd
+++ level.gd
@@ -9,7 +9,7 @@ signal midlevel_tween_finished
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	get_tree().paused = false
 	# Reset visibility of TileMapLayers (not sure if needed)
 	$TileMaps/Part1.show()
 	$TileMaps/Part1Goal.show()
@@ -35,3 +35,11 @@ func _on_player_reached_exit() -> void:
 	tween.tween_property($Player/Sprite2D, "modulate:a", 0, EXIT_TWEEN_DURATION)
 	await get_tree().create_timer(EXIT_TWEEN_DURATION * 2).timeout
 	get_tree().change_scene_to_packed(next_level)
+
+
+func _on_pause_level_reset() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_pause_level_quit() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
---
+++ pause.gd
@@ -0,0 +1,38 @@
+extends CanvasLayer
+
+signal level_reset
+signal level_quit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$PauseMenu.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# Pause the game engine and show PauseMenu.
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
+	$PauseMenu.show()
+
+
+# Close PauseMenu and unpause the game engine.
+func _on_resume_button_pressed() -> void:
+	$PauseMenu.hide()
+	get_tree().paused = false
+
+
+# Tell the current level to reload itself.
+func _on_reset_button_pressed() -> void:
+	level_reset.emit()
+	#get_tree().paused = false
+
+
+# Tell the current level to exit to Main.
+func _on_quit_button_pressed() -> void:
+	level_quit.emit()
+	#get_tree().paused = false
