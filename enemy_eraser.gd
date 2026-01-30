c7270079f4876cff6f7fa7e292d0d03ec3e20d5b
Fuzzlewuzzlekins <47910839+Fuzzlewuzzlekins@users.noreply.github.com>
2026-01-30 19:31:18 +0100

    Add animation sprites to eraser enemy

--- enemy_eraser.gd
+++ enemy_eraser.gd
@@ -19,5 +19,5 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 
-
+	$AnimatedSprite2D.play("walk")
 	move_and_slide()
